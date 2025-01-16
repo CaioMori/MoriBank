21783948ec067aa896fb4dec48e335b7224167b7
Caio Mori <caiomori22@gmail.com>
2025-01-16 08:59:01 -0300

    Initial Commit

---
+++ MoriBank/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MoriBank
+//
+//  Created by Caio Mori on 16/01/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MoriBank/MoriBankApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoriBankApp.swift
+//  MoriBank
+//
+//  Created by Caio Mori on 16/01/25.
+//
+
+import SwiftUI
+
+@main
+struct MoriBankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
